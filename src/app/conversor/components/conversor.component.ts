dfea9441d4b238e4bbe83c17352867181c29a538
Gerdon Mafra <gerdon_mafra@hotmail.com>
2021-04-08 20:52:01 -0300

    Adicionar Diretiva de validação de números

--- src/app/conversor/components/conversor.component.ts
+++ src/app/conversor/components/conversor.component.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+
+// Models
+import { Conversao } from '../models/conversao.model';
+import { ConversaoResponse } from '../models/conversao-response.model';
+import { Moeda } from '../models/moeda.model';
+
+// Services
+import { ConversorService } from '../services/conversor.service';
+import { MoedaService } from '../services/moeda.service';
 
 @Component({
   selector: 'app-conversor',
@@ -8,11 +18,48 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ConversorComponent implements OnInit {
 
+  @ViewChild("conversaoForm", { static: true }) conversaoForm: NgForm;
+
+  moedas: Moeda[];
+  conversao: Conversao;
+  possuiErro: boolean;
+  conversaoResponse: ConversaoResponse;
+
   constructor(
-    private http: HttpClient
+    private conversorService: ConversorService,
+    private http: HttpClient,
+    private moedaService: MoedaService,
   ) { }
 
   ngOnInit(): void {
+    this.moedas = this.moedaService.listarTodas();
+    this.init();
+  }
+
+  /**
+   * Efetua a chamada para a conversão dos valores.
+   *
+   * @return void
+   */
+   init(): void {
+  	this.conversao = new Conversao('EUR', 'BRL', null);
+  	this.possuiErro = false;
+  }
+
+  /**
+   * Efetua a chamada para a conversão dos valores.
+   *
+   * @return void
+   */
+  converter(): void {
+  	if (this.conversaoForm.form.valid) {
+  	  this.conversorService
+        .converter(this.conversao)
+  	  	.subscribe(
+  		    response => {this.conversaoResponse = response; console.log(response);},
+          error => this.possuiErro = true
+        );
+  	}
   }
 
 }
--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { ConversorComponent } from './components/conversor.component';
 
 import { ConversorService } from './services/conversor.service';
 import { MoedaService } from './services/moeda.service';
+import { NumeroDirective } from './directives/numero.directive';
 
 @NgModule({
   declarations: [
-    ConversorComponent
+    ConversorComponent,
+    NumeroDirective
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    HttpClientModule
   ],
   exports: [
     ConversorComponent
